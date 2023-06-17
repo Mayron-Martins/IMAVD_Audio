0c7c32b03f79af2262c180e20af1eba70b441bd3
Hithman_01 <64665904+Mayron-Martins@users.noreply.github.com>
2023-06-17 15:48:01 +0100

    Create Class Object

---
+++ Project_Audio/Model/Object.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Audio.Model
+{
+    public class Object
+    {
+        private Image shape;
+        private string type;
+
+        public Object(String shape)
+        {
+            this.shape = this.GenerateShape(shape);
+            this.type = shape;
+        }
+
+        private Image GenerateShape(String shape)
+        {
+            return null;
+        }
+    }
+}
